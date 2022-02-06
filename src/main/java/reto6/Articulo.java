c68e69b57c117c234c0abafa0d4e820a38eea779
Kelly Julieth Arango Henao <79645771+Kell2301@users.noreply.github.com>
2022-02-06 15:53:10 -0500

    Add files via upload

---
+++ src/main/java/reto6/Articulo.java
@@ -0,0 +1,43 @@
+package reto6;
+/**
+ * @author 
+ * Kelly Julieth Arango Henao
+ * Juan Camilo Rivera Avendaño
+ */
+public class Articulo {
+    private String nombre;
+    private String descripcion;
+    private double valor;
+
+    public Articulo(String nombre, String descripcion, double valor) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
+}
---
+++ src/main/java/reto6/Banco.java
@@ -0,0 +1,20 @@
+package reto6;
+/**
+ * @author 
+ * Kelly Julieth Arango Henao
+ * Juan Camilo Rivera Avendaño
+ */
+import java.util.Hashtable;
+
+public class Banco {
+    private Hashtable clientes = new Hashtable();
+
+    public Hashtable getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Hashtable clientes) {
+        this.clientes = clientes;
+    }
+    
+}
